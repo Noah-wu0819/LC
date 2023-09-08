8bf38e0e7a3975e1fe895ab32ec04ce94401ae8f
noah <wags0819@gmail.com>
2023-09-08 17:15:22 -0400

    Sliding Window Median

--- c11_TwoHeaps.java
+++ c11_TwoHeaps.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -43,10 +44,51 @@ public class c11_TwoHeaps {
     }
 
     //TODO Sliding Window Median (hard)
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>((o1, o2)->o1-o2);
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2)->o2-o1);
     public double[] findSlidingWindowMedian(int[] nums, int k) {
         double[] result = new double[nums.length - k + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (maxHeap.size() == 0 || nums[i] <= maxHeap.peek()){
+                maxHeap.add(nums[i]);
+            }else {
+                minHeap.add(nums[i]);
+            }
+            reBalanceHeap();
+            if (i - k + 1 >= 0){
+                if (maxHeap.size() == minHeap.size()){
+                    result[i - k + 1] = maxHeap.peek()/2.0 + minHeap.peek()/2.0;
+                }else {
+                    result[i - k + 1] = maxHeap.peek();
+                }
+
+                int remove = nums[i - k + 1];
+                if (remove <= maxHeap.peek()){
+                    maxHeap.remove(remove);
+                }else {
+                    minHeap.remove(remove);
+                }
+                reBalanceHeap();
+            }
+        }
+
         // TODO: Write your code here
         return result;
+    }
+
+    private void reBalanceHeap() {
+        if (maxHeap.size() > minHeap.size() + 1){
+            minHeap.add(maxHeap.poll());
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    //TODO Maximize Capital (hard)
+    public static int findMaximumCapital(int[] capital, int[] profits,int numberOfProjects, int initialCapital) {
+        // TODO: Write your code here  
+        return 0;
     }
 
 }
