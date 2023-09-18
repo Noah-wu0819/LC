30e7b9fa9856b408dff10b9a04c767e9f248f2de
noah <wags0819@gmail.com>
2023-09-18 14:07:34 -0400

    Update c13_Binary_Search.java

--- c13_Binary_Search.java
+++ c13_Binary_Search.java
@@ -161,7 +161,10 @@ public class c13_Binary_Search {
     //TODO Minimum Difference Element (medium)
     public static int searchMinDiffElement(int[] arr, int key) {
         // TODO: Write your code here
-
+        int minDif = Integer.MAX_VALUE;
+        int start = 0;
+        int end = arr.length - 1;
+        
         return -1;
     }
 }
