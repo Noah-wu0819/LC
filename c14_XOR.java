564d83d87b8bf5999ebc3ad573a26a28bb3d0db0
noah <wags0819@gmail.com>
2023-09-21 13:20:05 -0400

    Update c14_XOR.java

--- c14_XOR.java
+++ c14_XOR.java
@@ -3,3 +3,20 @@
 Taking XOR of a number with 0 returns the same number, e.g., 1 ^ 0 = 1 31 ^ 0 = 31
 XOR is Associative & Commutative, which means: (a ^ b) ^ c = a ^ (b ^ c) a ^ b = b ^ a
 * */
+
+
+class Solution {
+    //TODO Single Number (easy)
+    public static int findSingleNumber(int[] arr) {
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            num ^= arr[i];
+        }
+        return num;
+    }
+    //TODO Two Single Numbers (medium)
+    public static int[] findSingleNumbers(int[] nums) {
+        // TODO: Write your code here
+        return new int[] { 1, 1};
+    }
+}
