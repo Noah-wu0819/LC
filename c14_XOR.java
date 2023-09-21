1af51c1da8034b683bbc1c0d15bc2e34246b6141
noah <wags0819@gmail.com>
2023-09-21 13:13:48 -0400

    Create c14_XOR.java

---
+++ c14_XOR.java
@@ -0,0 +1,5 @@
+/*
+* Taking XOR of a number with itself returns 0, e.g., 1 ^ 1 = 0 29 ^ 29 = 0
+Taking XOR of a number with 0 returns the same number, e.g., 1 ^ 0 = 1 31 ^ 0 = 31
+XOR is Associative & Commutative, which means: (a ^ b) ^ c = a ^ (b ^ c) a ^ b = b ^ a
+* */
