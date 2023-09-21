431f3ffb1185729231277db299827173ed38d0bd
noah <wags0819@gmail.com>
2023-09-21 14:05:15 -0400

    Update c14_XOR.java

--- c14_XOR.java
+++ c14_XOR.java
@@ -17,6 +17,25 @@ class Solution {
     //TODO Two Single Numbers (medium)
     public static int[] findSingleNumbers(int[] nums) {
         // TODO: Write your code here
-        return new int[] { 1, 1};
+        int n1xn2 = 0;
+        int right = 1;
+        int n1 = 0;
+        int n2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            n1xn2 ^= nums[i];
+        }
+
+        while ((right & n1xn2) == 0){
+            right <<= 1;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] & right) != 0){
+                n1 ^= nums[i];
+            }else {
+                n2 ^= nums[i];
+            }
+        }
+        return new int[] { n1, n2};
     }
 }
