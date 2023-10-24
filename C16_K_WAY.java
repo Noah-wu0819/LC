661ed684e9d9214318f04076e200f37a39095e80
noah <wags0819@gmail.com>
2023-10-24 13:58:39 -0400

    Update C16_K_WAY.java

--- C16_K_WAY.java
+++ C16_K_WAY.java
@@ -1,6 +1,4 @@
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class C16_K_WAY {
     //TODO Merge K Sorted Lists (medium)
@@ -59,6 +57,7 @@ public class C16_K_WAY {
         int result = 0;
         // TODO: Write your code here
         PriorityQueue<Matrix> minHeap = new PriorityQueue<>((a,b)->matrix[a.row][a.col] - matrix[b.row][b.col]);
+
         for (int i = 0; i < matrix.length; i++) {
             minHeap.add(new Matrix(i, 0));
         }
@@ -100,6 +99,28 @@ public class C16_K_WAY {
             }
         }
         return new int[] { rangeStart, rangeEnd };
+    }
+
+    //TODO Problem Challenge 1: K Pairs with Largest Sums (hard)
+    public static List<List<Integer>> findKLargestPairs(int[] nums1, int[] nums2, int k) {
+        // TODO: Write your code here
+        PriorityQueue<List<Integer>> minHeap = new PriorityQueue<>((a,b)->(a.get(0)+a.get(1))-(b.get(0)+b.get(1)));
+
+        for (int i = 0; i < nums1.length && i < k; i++) {
+            for (int j = 0; j < nums2.length && j < k; j++) {
+                if (minHeap.size() < k){
+                    minHeap.add(Arrays.asList(nums1[i], nums2[j]));
+                }else{
+                    if (nums1[i] + nums2[j] < minHeap.peek().get(0) + minHeap.peek().get(1)) break;
+                    else {
+                        minHeap.poll();
+                        minHeap.add(Arrays.asList(nums1[i], nums2[j]));
+                    }
+                }
+
+            }
+        }
+        return new ArrayList<>(minHeap);
     }
 }
 class Matrix{
