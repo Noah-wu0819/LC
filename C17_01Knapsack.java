126b5af779615d1f445c09e75189a0d3bf699815
noah <wags0819@gmail.com>
2023-11-04 15:28:22 -0400

    Update C17_01Knapsack.java

--- C17_01Knapsack.java
+++ C17_01Knapsack.java
@@ -39,7 +39,20 @@ public class C17_01Knapsack {
         return dp[profits.length-1][capacity];
     }
 
-   
+    private int knapsackRecursive(Integer[][] dp, int[] profits, int[] weights, int capacity, int currentIndex) {
+        if (capacity <= 0 || currentIndex >= profits.length) return 0;
+        if (dp[currentIndex][capacity] != null) return dp[currentIndex][capacity];
+        //使用 Integer[][]排除null的情况，int[][]不能判断null
+        int profit1 = 0;
+        //注意 >=而不是>
+        if (capacity >= weights[currentIndex]){
+            profit1 = profits[currentIndex] + knapsackRecursive(dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+        }
+        //这里是去掉当前index的物品，所以不能capacity - weights[currentIndex]，只留capacity
+        int profit2 = knapsackRecursive(dp, profits, weights, capacity, currentIndex + 1);
+        dp[currentIndex][capacity] = Math.max(profit2, profit1);
+        return dp[currentIndex][capacity];
+    }
 
     private void printSelectedElements(int[][] dp, int[] weights, int[] profits, int capacity){
         List<Integer> elementsWeights = new ArrayList<>();
